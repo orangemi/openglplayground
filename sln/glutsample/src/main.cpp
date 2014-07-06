acc75bbd53eb23e07c58bbbcafcbbe7acd393a1a
Orange Mi <orgmi@sina.com>
2014-07-06 23:00:40 +0800

    add image library

--- sln/glutsample/src/main.cpp
+++ sln/glutsample/src/main.cpp
@@ -1,6 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+
 #include <GL/glew.h>
 #include <GL/glut.h>
-#include <stdio.h>
+#include <png.h>
+
+
+GLubyte *textureImage;
 
 void changeSize(int w, int h) {
 
@@ -31,27 +38,106 @@ float angle = 0.0f;
 
 void renderScene(void) {
 
-	// Clear Color and Depth Buffers
+	glLoadIdentity();
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	// Reset transformations
-	glLoadIdentity();
-	// Set the camera
-	gluLookAt(	0.0f, 0.0f, 10.0f,
-				0.0f, 0.0f,  0.0f,
-				0.0f, 1.0f,  0.0f);
-
-	glRotatef(angle, 0.0f, 1.0f, 0.0f);
-
-	glBegin(GL_TRIANGLES);
-		glVertex3f(-2.0f,-2.0f, 0.0f);
-		glVertex3f( 2.0f, 0.0f, 0.0);
-		glVertex3f( 0.0f, 2.0f, 0.0);
+	glBegin(GL_QUADS);
+		glTexCoord2f(0.0, 0.0); glVertex3f(-1.0, -1.0, 0.0);
+		glTexCoord2f(0.0, 1.0);	glVertex3f(-1.0,  1.0, 0.0);
+		glTexCoord2f(1.0, 1.0);	glVertex3f( 1.0,  1.0, 0.0);
+		glTexCoord2f(1.0, 0.0);	glVertex3f( 1.0, -1.0, 0.0);
 	glEnd();
 
-	angle+=0.1f;
-
 	glutSwapBuffers();
+}
+
+
+
+bool loadPngImage(const char* name, int &outWitdh, int &outHeight, bool &outHasAlpha, GLubyte **outData) {
+	png_structp png_ptr;
+	png_infop info_ptr;
+	unsigned int read = 0;
+	int color_type, interlace_type;
+	FILE *fp;
+
+	if ((fp = fopen(name, "rb")) == NULL) {
+		std::cout << "fopen" << std::endl;
+		return false;
+	}
+
+	png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+	if (png_ptr == NULL) {
+		std::cout << "png_ptr" << std::endl;
+		fclose(fp);
+		return false;
+	}
+
+	info_ptr = png_create_info_struct(png_ptr);
+	if (!info_ptr) {
+		std::cout << "info_ptr" << std::endl;
+		fclose(fp);
+		png_destroy_read_struct(&png_ptr, NULL, NULL);
+		return false;
+	}
+
+	if (setjmp(png_jmpbuf(png_ptr))) {
+		std::cout << "setjmp" << std::endl;
+		fclose(fp);
+		png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
+		return false;
+	}
+
+	png_init_io(png_ptr, fp);
+	png_set_sig_bytes(png_ptr, read);
+	png_read_png(png_ptr, info_ptr, PNG_TRANSFORM_STRIP_16 | PNG_TRANSFORM_PACKING | PNG_TRANSFORM_EXPAND, NULL);
+	png_uint_32 width, height;
+	int bit_depth;
+	png_get_IHDR(png_ptr, info_ptr, &width, &height, &bit_depth, &color_type, &interlace_type, NULL, NULL);
+	outWitdh = width;
+	outHeight = height;
+
+	unsigned int row_bytes = png_get_rowbytes(png_ptr, info_ptr);
+	*outData = (unsigned char*) malloc(row_bytes *outHeight);
+	png_bytepp row_pointers = png_get_rows(png_ptr, info_ptr);
+
+	for (int i = 0; i < outHeight; i++) {
+		memcpy(*outData + (row_bytes * (outHeight - 1 - i)), row_pointers[i], row_bytes);
+	}
+
+	png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
+
+	return true;
+}
+
+void init() {
+
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glEnable(GL_DEPTH_TEST);
+	// The following two lines enable semi transparent
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	char* filename = "logo.png";
+	int width, height;
+	bool hasAlpha;
+	bool success = loadPngImage(filename, width, height, hasAlpha, &textureImage);
+	if (!success) {
+		std::cout << "Unable to load " << filename << std::endl;
+		return;
+	}
+
+	hasAlpha = false;
+	std::cout << "Image loaded " << width << " " << height << " alpha " << hasAlpha << std::endl;
+
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	glTexImage2D(GL_TEXTURE_2D, 0, hasAlpha ? GL_RGBA8 : GL_RGB8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, textureImage);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_CLAMP);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_CLAMP);
+	glEnable(GL_TEXTURE_2D);
+	glShadeModel(GL_FLAT);
+
 }
 
 int main(int argc, char **argv) {
@@ -63,13 +149,15 @@ int main(int argc, char **argv) {
 	glutInitWindowSize(320,320);
 	glutCreateWindow("Lighthouse3D- GLUT Tutorial");
 
+	init();
+
 	// register callbacks
 	glutDisplayFunc(renderScene);
-	glutReshapeFunc(changeSize);
+//	glutReshapeFunc(changeSize);
 	glutIdleFunc(renderScene);
 
 	// enter GLUT event processing cycle
 	glutMainLoop();
 
-	return 1;
+	return 0;
 }
